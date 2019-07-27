63da4d5b5c01009c6f2cf2df771b17b3c5298a72
Benjamin Rodriguez <ben@itnachos.com>
2019-07-27 16:17:46 -0500

    step 16

--- src/app/services/todo-list.service.ts
+++ src/app/services/todo-list.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoListService {
+  private todoList: TodoItem[] = [
+    { title: 'install NodeJS' },
+    { title: 'install Angular CLI' },
+    { title: 'create new app' },
+    { title: 'serve app' },
+    { title: 'develop app' },
+    { title: 'deploy app' }
+  ];
+  constructor() {}
 
-  constructor() { }
+  getTodoList() {
+    return this.todoList;
+  }
+  addItem(item: TodoItem) {
+    this.todoList.push(item);
+  }
 }
